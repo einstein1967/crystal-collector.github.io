50ad80761cc83f6ee0cc7d6b4a9ded1079c39e96
einstein1967 <bootcamp.ejb@gmail.com>
2019-05-04 17:29:32 -0500

    turn in push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,55 @@
-var wins = 0;
-var losses = 0;
-var current = 0;
-var totalScore = 0;
-var clicks = 0;
-var totalScore = 0;
-var target = 0;
-
 $(document).ready(function () {
 
-    var amber = Math.floor(Math.random() * 12) + 1;
-    var emerald = Math.floor(Math.random() * 12) + 1;
-    var ruby = Math.floor(Math.random() * 12) + 1;
-    var sapphire = Math.floor(Math.random() * 12) + 1;
-    target = Math.floor(Math.random()*102)+19;
+    var wins = 0;
+    var losses = 0;
+    var current = 0;
+    var totalScore = 0;
+    var clicks = 0;
+    var totalScore = 0;
+    var target = 0;
+    var amber;
+    var emerald;
+    var ruby;
+    var sapphire;
+    var gemValues = [];
 
-    console.log("amber = " + amber);
-    console.log("emerald = " + emerald);
-    console.log("ruby = " + ruby);
-    console.log("sapphire = " + sapphire);
+    amber = Math.floor(Math.random() * 12) + 1;
+    emerald = Math.floor(Math.random() * 12) + 1;
+    ruby = Math.floor(Math.random() * 12) + 1;
+    sapphire = Math.floor(Math.random() * 12) + 1;
+    target = Math.floor(Math.random() * 102) + 19;
+
+    console.log(amber);
+    console.log(emerald);
+    console.log(ruby);
+    console.log(sapphire);
 
 
-    var reset = function() {
-        var amber = Math.floor(Math.random() * 12) + 1;
-        var emerald = Math.floor(Math.random() * 12) + 1;
-        var ruby = Math.floor(Math.random() * 12) + 1;
-        var sapphire = Math.floor(Math.random() * 12) + 1;
-        target = Math.floor(Math.random()*102)+19;
+    var reset = function () {
+        amber = Math.floor(Math.random() * 12) + 1;
+        emerald = Math.floor(Math.random() * 12) + 1;
+        ruby = Math.floor(Math.random() * 12) + 1;
+        sapphire = Math.floor(Math.random() * 12) + 1;
+        target = Math.floor(Math.random() * 102) + 19;
         totalScore = 0;
+        console.log(amber);
+        console.log(emerald);
+        console.log(ruby);
+        console.log(sapphire);
         $("#current").text(totalScore);
         $("#target").text(target);
-        console.log("amber = " + amber);
-        console.log("emerald = " + emerald);
-        console.log("ruby = " + ruby);
-        console.log("sapphire = " + sapphire);    
     }
 
-    var theEnd = function() {
-        if(totalScore>target) {
-            var result="You lose.";
+    var theEnd = function () {
+        if (totalScore > target) {
+            var result = "You lose.";
             console.log(result);
             $("#win-lose").text(result);
             losses++;
             $("#losses").text(losses);
             reset();
-        }else if(totalScore===target) {
-            var result="You win.";
+        } else if (totalScore === target) {
+            var result = "You win.";
             console.log(result);
             $("#win-lose").text(result);
             wins++;
@@ -61,7 +66,7 @@ $(document).ready(function () {
         theEnd();
     });
 
-$("#gem-2").click(function () {
+    $("#gem-2").click(function () {
         totalScore += emerald;
         $("#current").text(totalScore);
         console.log("totalScore =" + totalScore);
